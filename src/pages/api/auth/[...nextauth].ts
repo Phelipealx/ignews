4530c85423f1c81217a7d3c925b68177ed8c897a
Phelipe Magalhães <phelipeamagalhaes@gmail.com>
2022-03-18 19:37:08 -0300

    implementa query create user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -12,6 +14,24 @@ export default NextAuth({
         },
       },
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
