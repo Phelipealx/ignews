325f9757be5b12fca8878ebe099d76024d9767ea
Phelipealx <phelipeamagalhaes@gmail.com>
2022-08-05 20:34:32 -0300

    adiciona test coverage

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|scss|sass)$": "identity-obj-proxy",
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx",
+  ],
+  coverageReporters: ["lcov", "json"],
 };
